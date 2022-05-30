efc2419e476951f2f4e4b4ecc11b09c521cb0411
erik schievenin <erik.schievenin@gmail.com>
2022-05-30 18:54:05 +0200

    Define admin + guests routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+// Rotte per l'autenticazione
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+    });
+
+
+    Route::get("{any?}", function(){
+        return view('guest.home');
+    })->where("any", ".*");
